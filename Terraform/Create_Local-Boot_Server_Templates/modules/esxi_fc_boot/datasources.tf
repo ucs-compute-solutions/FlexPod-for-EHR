a95ff2d05ab3d81ebde56593657f3b58817bb1ff
Ken Corkins <kecorkin@cisco.com>
2022-02-24 13:49:02 -0600

    Updated to add 2nd vHBA for NVME

--- Terraform/Create_Local-Boot_Server_Templates/modules/esxi_fc_boot/datasources.tf
+++ Terraform/Create_Local-Boot_Server_Templates/modules/esxi_fc_boot/datasources.tf
@@ -4,5 +4,5 @@ data "intersight_iam_end_point_role" "admin_role" {
 }
 data "intersight_iam_end_point_user_policy" "user_policy1" {
 }
-data "intersight_storage_storage_policy" "hc_storage_policy" {
-}
+#data "intersight_storage_storage_policy" "hc_storage_policy" {
+#}
---
+++ Terraform/Create_SAN-Boot_Server_Templates/modules/nvme_pool/variables.tf
@@ -0,0 +1,23 @@
+variable "name_of_nvme_pool" {
+}
+
+
+variable "description_of_nvme_pool" {
+  default = ""
+}
+
+
+variable "type_of_nvme_pool" {
+}
+
+
+variable "nvme_pool_start" {
+}
+
+
+variable "size_of_nvme_pool" {
+}
+
+
+variable "org_moid" {
+}
